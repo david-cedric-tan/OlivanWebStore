8f03a93cc5c008f22c85faca0a0cdcdee93a445b
ANTiDOTE-21 <dtan7448@uni.sydney.edu.au>
2025-12-16 18:47:36 +0800

    skeleton v1

---
+++ frontend/components/product-grid/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProductGrid() {
+  return (
+    <div className="text-shadow-cyan-500 flex justify-center items-center text-amber-300 font-bold">
+      This is the product-grid component
+    </div>
+  );
+}
